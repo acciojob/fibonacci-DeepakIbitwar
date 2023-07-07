6da50cab44664eabc1df192b401e5f483f69fb6a
DeepakIbitwar <127574226+DeepakIbitwar@users.noreply.github.com>
2023-07-08 00:28:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
 // your code here
+	let a=0;
+	let b=1;
+
+	for(let i=0;i<num;i++){
+		console.log(a);
+		let c=a+b;
+		
+		a=b;
+		b=c;
+		
+	}
 }
 
 module.exports = fibonacci;
