52add1977e8005a29e1bad06e3610fa886596336
DeepakIbitwar <127574226+DeepakIbitwar@users.noreply.github.com>
2023-07-08 00:31:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function fibonacci(num) {
 // your code here
-	let a=0;
-	let b=1;
+	var a=0;
+	var b=1;
 
 	for(let i=0;i<num;i++){
 		console.log(a);
-		let c=a+b;
+		var c=a+b;
 		
 		a=b;
 		b=c;
